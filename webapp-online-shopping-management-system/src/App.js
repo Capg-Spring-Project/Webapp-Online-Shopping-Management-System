e97bf83401b36543fbca03f94b9be7737d80da82
Shubham99293 <shubham99348@gmail.com>
2022-07-20 12:49:01 +0530

    footer.js

--- webapp-online-shopping-management-system/src/App.js
+++ webapp-online-shopping-management-system/src/App.js
@@ -4,16 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import BuyMedicineComponent from './component/medicine/BuyMedicineComponent';
 import LoginComponent from './component/authentication/LoginComponent';
 import SignUpComponent from './component/authentication/SignUpComponent';
-
-
+import Footer  from './Footer';
+// import Menu from './Menu';
 function App() {
     return (
         <div className="App">
-
+            
             <ListMedicineComponent />
             <BuyMedicineComponent/>
             <LoginComponent />
             <SignUpComponent/>
+            <Footer></Footer>
         </div>
     );
 }
